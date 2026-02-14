6313eba92770b544dcd7b1ddab523ec17623469f
shaik subhani <subhani8189@gmail.com>
2026-02-14 16:51:56 +0530

    Update S3 bucket name in backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "doc-summarizer-upload-20260214103915828700000001" # REPLACE with the bucket name from Phase 1, Step 2
+    bucket = "subhani9394" # REPLACE with the bucket name from Phase 1, Step 2
     key    = "doc-summarizer/terraform.tfstate"
     region = "us-east-1"
   }
